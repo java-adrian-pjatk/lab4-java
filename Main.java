58f73681f40ac5a90b945ebbc41836e971dbefff
forqediv <48859399+forqediv@users.noreply.github.com>
2019-04-06 15:50:05 +0200

    test czy dziala

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        InvoiceItem test = new InvoiceItem("1","dd", 6, 5.4);
+        System.out.println(test.getID());
+
+    }
+}
