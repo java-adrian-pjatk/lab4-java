c18f67776bbcce22fc3ac8cbf8cbbe5aa0ba9795
Adrian M <s19850@pjwstk.edu.pl>
2019-04-06 15:58:13 +0200

    Create Main 01.java

---
+++ Main 01.java
@@ -0,0 +1,52 @@
+    public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+    public int getID()
+        {
+           return id;
+        }
+
+public getFirstName
+        {
+        return firstName;
+        }
+public LastName():String;
+        {
+        return lastName;
+        }
+public getName():String;
+        {
+        return firstName; lastName;
+        }
+public getSalary():int
+        {
+
+        }
+public setSalary(salary:int):void
+        {
+
+        }
+public gaetAnnualSalary():int
+        {
+
+        }
+public raiseSalary (int percent):int
+        {
+
+        }
+public toString():String;
+        {
+        return Employee [id, name, salary]
+        }
+
