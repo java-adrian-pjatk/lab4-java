ee4c0f45a18ffa9446463c90cb9c6bc6ce971a07
adriancpp <47180493+adriancpp@users.noreply.github.com>
2019-04-27 06:28:50 +0200

    Create Main 04.java
    complete

---
+++ Main 05.java
@@ -0,0 +1,97 @@
+// autorzy: Adrian Witkowski
+// data wykonania: 27.04.2019
+// cel: Time
+
+public class Time
+{
+    private int hour, second, minute;
+
+    public Time(int hour, int minute, int second)
+    {
+        this.hour = hour;
+        this.second = second;
+        this.minute = minute;
+    }
+
+    public int getHour()
+    {
+        return this.hour;
+    }
+
+    public int getMinute()
+    {
+        return this.minute;
+    }
+
+    public int getSecond()
+    {
+        return this.second;
+    }
+
+    public void setHour(int hour)
+    {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute)
+    {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second)
+    {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second)
+    {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toString()
+    {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    public Time nextSecond()
+    {
+        second++;
+        if (second >= 60)
+        {
+            second = 0;
+            minute++;
+            if (minute >= 60)
+            {
+                minute = 0;
+                hour++;
+                if (hour >= 24)
+                {
+                    hour = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    public Time previousSecond()
+    {
+        second--;
+        if (second < 0)
+        {
+            second = 59;
+            minute--;
+            if (minute < 0)
+            {
+                minute = 59;
+                hour--;
+                if (hour < 0)
+                {
+                    hour = 23;
+                }
+            }
+        }
+        return this;
+    }
+}
